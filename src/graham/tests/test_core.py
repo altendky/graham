9d280c87c5f4e8dd18e718bdb37a422a6270dd0d
Kyle Altendorf <sda@fstab.net>
2017-10-09 21:26:29 -0400

    Add test_dumps()

--- src/graham/tests/test_core.py
+++ src/graham/tests/test_core.py
@@ -1,4 +1,7 @@
+import textwrap
+
 import attr
+import marshmallow
 import pytest
 
 import graham
@@ -11,3 +14,27 @@ def test_missing_metadata():
         @graham.set_type('test')
         class Test:
             test = attr.ib()
+
+
+def test_dumps():
+    @graham.schemify
+    @attr.s
+    @graham.set_type('test')
+    class Test:
+        test = attr.ib(
+            metadata=graham.create_metadata(
+                field=marshmallow.fields.String()
+            ),
+        )
+
+    test = Test(test='test')
+
+    raw = graham.core.dumps(test).data
+    assert raw == '{"test": "test", "_type": "test"}'
+
+    indented = graham.core.dumps(test, indent=4).data
+    assert indented == textwrap.dedent('''\
+    {
+        "test": "test",
+        "_type": "test"
+    }''')
