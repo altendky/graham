a49e972911485b78e1f7b0173cf9bf1a27db44fd
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-11-29 22:24:06 -0500

    Fill out for py27, 34, 35, 36

--- setup.py
+++ setup.py
@@ -14,7 +14,12 @@ setuptools.setup(
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': [
